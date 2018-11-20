1c5f203e9a6c3f88421ec48a0bdf37bf56c08244
Kate Stroi <catia.stroi@gmail.com>
2018-11-20 15:45:06 +0200

    added

--- webpack.mix.js
+++ webpack.mix.js
@@ -36,7 +36,7 @@ mix.webpackConfig({
  |
  */
 
-mix.browserSync('barebones.test');
+mix.browserSync('pilatest.local');
 
 mix.js('src/scripts/main.js', 'dist/scripts/')
    .sass('src/styles/main.scss', 'dist/styles/')
