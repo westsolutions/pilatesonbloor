54907e08a0e858f4e6d0cfa4eb4d34ed0a276ec8
Kate Stroi <catia.stroi@gmail.com>
2018-11-23 17:02:54 +0200

    added styles and responsive

--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,7 @@
 <?php partial('head') ?>
 
 <body >
-
+	<div class="bg-top" style="background-image:url(<?= asset('images/image.svg') ?>)"></div>
 	<header class="wrapper-header">
 		<div class="container">
 			<nav class="navbar navbar-expand-lg d-flex justify-content-between">
@@ -60,7 +60,7 @@
 				</div>
 				<div class="col-12 col-md-4 ">
 					<div class="wrapper-img">
-						<img src="<?= asset('images/b.jpg') ?>" alt="Breath-Focused">
+						<img src="<?= asset('images/b.png') ?>" alt="Breath-Focused">
 						<p>Breath-Focused</p>
 					</div>
 				</div>
@@ -80,7 +80,7 @@
 		<div class="container">		
 			<div class="our-team-title text-center">
 				<h3>Our team</h3>
-				<p>Our Pilates approach is <strong><em>safe</em></strong>, <strong><em>stable</em></strong> and <strong><em>functional</em></strong>, taught by experienced Instructor Trainers each with over <strong><em>2 decades</em></strong> of functional movement based practices.</p>
+				<h4>Our Pilates approach is <strong><em>safe</em></strong>, <strong><em>stable</em></strong> and <strong><em>functional</em></strong>, taught by experienced Instructor Trainers each with over <strong><em>2 decades</em></strong> of functional movement based practices.</h4>
 			</div>
 			<div class="our-team-people">
 				<div class="our-team-left d-flex">
@@ -88,14 +88,14 @@
 						<img src="<?= asset('images/sarah-koekkoek.png') ?>" alt="Sarah Koekkoek">
 					</div>
 					<div class="container-text text-left ">
-						<h5>Sarah Koekkoek</h5>
-						<p>Sarah Koekkoek is an independent dance artist, educator and choreographer. She was born in Burlington, Ontario and trained in the Professional Division at Canada’s Royal Winnipeg Ballet School. She has danced with The Washington Ballet, Canada’s Ballet Jorgen, Les Grands Ballets Canadiens, The Royal Winnipeg Ballet and The Chimera Project as well as participating in several dance residencies at The Banff Centre for the Arts. 
-						Sarah has been practicing Pilates since 2007 and in 2012 became a Licensed Provider of the <strong>Fletchers Pilates</strong> Method. Sarah continues her Pilates and Movement education through courses and workshops and in January 2019 will begin a three part Movement for Trauma certification with Jane Clapp. </p>
+						<h6 >Sarah Koekkoek</h6>
+						<p>Sarah Koekkoek is an independent dance artist, educator and choreographer. She was born in Burlington, Ontario and trained in the Professional Division at Canada’s Royal Winnipeg Ballet School. She has danced with The Washington Ballet, Canada’s Ballet Jorgen, Les Grands Ballets Canadiens, The Royal Winnipeg Ballet and The Chimera Project as well as participating in several dance residencies at The Banff Centre for the Arts.</p>
+						<p>Sarah has been practicing Pilates since 2007 and in 2012 became a Licensed Provider of the <strong>Fletchers Pilates</strong> Method. Sarah continues her Pilates and Movement education through courses and workshops and in January 2019 will begin a three part Movement for Trauma certification with Jane Clapp. </p>
 					</div>
 				</div>
 				<div class="our-team-right d-flex">
 					<div class="container-text d-flex flex-column text-right justify-content-center ">
-						<h5>Deanna Villa</h5>
+						<h6>Deanna Villa</h6>
 						<p>Deanna has been practicing Pilates, Yoga, Thai Massage and meditation since 1998.  Her passion and expertise of movement, stillness, strengthening and lengthening have organically spawned, merged and blossomed beautifully by these movement healing modalities.</p>
 						<p><strong>Pilates Mat Certified</strong> — Holistic Bodyworx Trained <strong>Reformer 1 &amp; 2 and ISP</strong> — Injuries &amp; Special Populations: STOTT PilatesTrained</p>
 					</div>
@@ -111,7 +111,7 @@
 		<div class="container">
 			<blockquote class="text-center">
 				<p>&quot;I have always cherished my time on the mat. The Pilates reformer element has opened, enhanced and expanded the Pilates experience for me to new dimensions and depths, so much that we are honoured to be launching our boutique Pilates on Bloor studio, situated inside our Thai Massage Toronto studio, opening January 2, 2019, or earlier&quot;</p>
-				<footer>Deanna Villa</footer>
+				<footer><h6>Deanna Villa</h6></footer>
 			</blockquote>
 		</div>
 	</section>
@@ -123,7 +123,7 @@
 				<h3>Classes</h3>
 				<p>Our classes ensure that your <strong><em>performance goals</em></strong> are met in a <strong><em>peaceful</em></strong> and <strong><em>grounded</em></strong> environment. Stable, methodical and focused to dynamic and precise active flow.</p>
 			</div>
-			<div class="row">
+			<div class="row row-eq-height">
 				<div class="col-12 col-md-4 text-center">
 					<div class="card">
 						<img class="card-img-top" src="<?= asset('images/istockphoto-846236610-1024x1024.jpg') ?>" alt="Card image cap">
@@ -169,66 +169,68 @@
 
 	<section class="wrapper-prising">
 		<div class="container text-center">
-			<table class="table table-sm">
-				<thead>
-			    	<tr>
-			    		<th scope="col"></th>
-			      		<th scope="col">Private</th>
-			      		<th scope="col">Semi-private&ast;<br/>(2 persons)</th>
-			      		<th scope="col">group<br/>reformer</th>
-			      		<th scope="col">group mat</th>
-			    	</tr>
-				</thead>
-				<tbody>
-				    <tr>
-				    	<th scope="row" rowspan="3" bgcolor="#FBF0DB">Intro</th>
-				    	<td class="border-r"><strong><em>Intro Session</em></strong><br/>$63</td>
-				    	<td class="border-r bg-gray"><strong><em>Intro Session</em></strong><br/>$40</td>
-				    	<td class="border-r"><strong><em>Intro Session</em></strong><br/>$25</td>
-				    	<td class="border-r bg-gray"><strong><em>Intro Session</em></strong><br/>$18</td>
-				    </tr>
-				    <tr>
-				    	<td class="border-r"><strong><em>Intro 2 Sessions</em></strong><br/>$108 <span>($54/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>Intro 3 Sessions</em></strong><br/>$108 <span>($36/ea.)</span></td>
-				    	<td class="border-r"><strong><em>Intro 4 Sessions</em></strong><br/>$108 <span>($27/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>Intro 6 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
-				    </tr>
-				    <tr class="border-bottom">
-				    	<td class="border-r"><strong><em>Intro 18 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>Intro 30 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
-				    	<td class="border-r"><strong><em>Intro 40 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>Intro 60 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
-				    </tr>
-				    <tr>
-				    	<th scope="row" rowspan="4" bgcolor="#FBF0DB">Regular</th>
-				    	<td class="border-r"><strong><em>1 Session</em></strong><br/>$90</td>
-				    	<td class="border-r bg-gray"><strong><em>1 Session</em></strong><br/>$50</td>
-				    	<td class="border-r"><strong><em>1 Session</em></strong><br/>$35</td>
-				    	<td class="border-r bg-gray"><strong><em>1 Session</em></strong><br/>$23</td>
-				    </tr>
-				    <tr>
-				    	<td class="border-r"><strong><em>5 Sessions</em></strong><br/>$425 <span>($85/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>5 Sessions</em></strong><br/>$240 <span>($48/ea.)</span></td>
-				    	<td class="border-r"><strong><em>5 Sessions</em></strong><br/>$165 <span>($33/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>5 Sessions</em></strong><br/>$110 <span>($22/ea.)</span></td>
-				    </tr>
-				    <tr>
-				    	<td class="border-r"><strong><em>Intro 10 Sessions</em></strong><br/>$800 ($80/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>10 Sessions</em></strong><br/>$450 <span>($45/ea.)</span></td>
-				    	<td class="border-r"><strong><em>10 Sessions</em></strong><br/>$300 <span>($30/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>10 Sessions</em></strong><br/>$210 <span>($21/ea.)</span></td>
-				    </tr>
-				    <tr class="border-bottom">
-				    	<td class="border-r"><strong><em>Intro 30 Sessions</em></strong><br/>$2250 <span>($75/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>30 Sessions</em></strong><br/>$1200 <span>($40/ea.)</span></td>
-				    	<td class="border-r"><strong><em>30 Sessions</em></strong><br/>$810 <span>($27/ea.)</span></td>
-				    	<td class="border-r bg-gray"><strong><em>30 Sessions</em></strong><br/>$600 <span>($20/ea.)</span></td>
-				    </tr>
-				</tbody>
-			</table>
+			<div class="table-responsive">
+				<table class="table">
+					<thead>
+				    	<tr>
+				    		<th scope="col" class="bg-int"></th>
+				      		<th scope="col" class="br-radius-l">Private</th>
+				      		<th scope="col">Semi-private&ast;<br/>(2 persons)</th>
+				      		<th scope="col">group<br/>reformer</th>
+				      		<th scope="col" class="br-radius-r">group mat</th>
+				    	</tr>
+					</thead>
+					<tbody>
+					    <tr>
+					    	<th scope="row" rowspan="3" bgcolor="#FBF0DB" class="intro" style="background-image:url(<?= asset('images/intro.png') ?>)"></th>
+					    	<td class="border-r"><strong><em>Intro Session</em></strong><br/>$63</td>
+					    	<td class="border-r bg-white"><strong><em>Intro Session</em></strong><br/>$40</td>
+					    	<td class="border-r"><strong><em>Intro Session</em></strong><br/>$25</td>
+					    	<td class="border-r bg-white"><strong><em>Intro Session</em></strong><br/>$18</td>
+					    </tr>
+					    <tr>
+					    	<td class="border-r"><strong><em>Intro 2 Sessions</em></strong><br/>$108 <span>($54/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>Intro 3 Sessions</em></strong><br/>$108 <span>($36/ea.)</span></td>
+					    	<td class="border-r"><strong><em>Intro 4 Sessions</em></strong><br/>$108 <span>($27/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>Intro 6 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
+					    </tr>
+					    <tr class="border-bottom">
+					    	<td class="border-r"><strong><em>Intro 18 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>Intro 30 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
+					    	<td class="border-r"><strong><em>Intro 40 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>Intro 60 Sessions</em></strong><br/>$108 <span>($18/ea.)</span></td>
+					    </tr>
+					    <tr>
+					    	<th scope="row" rowspan="4" bgcolor="#FBF0DB" class="regular" class="intro" style="background-image:url(<?= asset('images/regular.png') ?>)"></th>
+					    	<td class="border-r"><strong><em>1 Session</em></strong><br/>$90</td>
+					    	<td class="border-r bg-white"><strong><em>1 Session</em></strong><br/>$50</td>
+					    	<td class="border-r"><strong><em>1 Session</em></strong><br/>$35</td>
+					    	<td class="border-r bg-white"><strong><em>1 Session</em></strong><br/>$23</td>
+					    </tr>
+					    <tr>
+					    	<td class="border-r"><strong><em>5 Sessions</em></strong><br/>$425 <span>($85/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>5 Sessions</em></strong><br/>$240 <span>($48/ea.)</span></td>
+					    	<td class="border-r"><strong><em>5 Sessions</em></strong><br/>$165 <span>($33/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>5 Sessions</em></strong><br/>$110 <span>($22/ea.)</span></td>
+					    </tr>
+					    <tr>
+					    	<td class="border-r"><strong><em>Intro 10 Sessions</em></strong><br/>$800 ($80/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>10 Sessions</em></strong><br/>$450 <span>($45/ea.)</span></td>
+					    	<td class="border-r"><strong><em>10 Sessions</em></strong><br/>$300 <span>($30/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>10 Sessions</em></strong><br/>$210 <span>($21/ea.)</span></td>
+					    </tr>
+					    <tr class="border-bottom">
+					    	<td class="border-r"><strong><em>Intro 30 Sessions</em></strong><br/>$2250 <span>($75/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>30 Sessions</em></strong><br/>$1200 <span>($40/ea.)</span></td>
+					    	<td class="border-r"><strong><em>30 Sessions</em></strong><br/>$810 <span>($27/ea.)</span></td>
+					    	<td class="border-r bg-white"><strong><em>30 Sessions</em></strong><br/>$600 <span>($20/ea.)</span></td>
+					    </tr>
+					</tbody>
+				</table>
+			</div>
 			<p><strong><em>&ast; If only 1 person shows up for a booked semi-private, the private fee will apply.</em></strong></p>
 			<p>We require 24 hours’ notice for cancellations for all private, semi-private and group sessions, otherwise, regular charges will apply.<br/><strong><em>To cancel</em></strong> log into your account and cancel the booking.</p>
-			<p>We look forward to seeing you on the mat!</p>
+			<h4>We look forward to seeing you on the mat!</h4>
 			<a class="btn btn-lg btn-outline-primary" href="/">Register today</a>
 		</div>
 	</section>
@@ -236,17 +238,19 @@
 	<section class="wrapper-form" id="contact">
 		<div class="container">
 			<div class="row">
-				<div class="col-3">
-					<h4>Contact</h4>
-					<ul>
-						<li>664 Bloor St West , 2nd FL<br/>Toronto ON<br/>M6G 1L2</li>
-						<li><strong><em>Located inside Thai Massage Toronto Studio</em></strong></li>
-						<li>(416) 533 7419</li>
-						<li>info@pilatesonbloor.com</li>
-					</ul>
+				<div class="col-5">
+					<div class="wrap-contact">
+						<h6>Contact</h6>
+						<ul>
+							<li><p>664 Bloor St West , 2nd FL<br/>Toronto ON<br/>M6G 1L2</p></li>
+							<li><p><strong><em>Located inside Thai Massage Toronto Studio</em></strong></p></li>
+							<li><p>(416) 533 7419</p></li>
+							<li><p>info@pilatesonbloor.com</p></li>
+						</ul>
+					</div>
 				</div>
 				<div class="col-4">
-					<h4>Questions?</h4>
+					<h6>Questions?</h6>
 					<p>Fill in and submit the form and we will get back to you as quickly as possible</p>
 					<form>
 						<div class="form-row">
@@ -258,9 +262,9 @@
 					    	</div>
 						</div>
 						<div class="form-group">
-					        <textarea class="form-control" rows="3"></textarea>
+					        <textarea class="form-control" rows="3" placeholder="Message"></textarea>
 						</div>
-					  <button type="submit" class="btn btn-lg btn-primary">Learn more</button> 
+					  <button type="submit" class="btn btn-md btn-primary">Learn more</button> 
 					</form>
 				</div>
 				<div class="col-5">
@@ -268,7 +272,7 @@
 			</div>
 		</div>
 	</section>
-	<footer>
+	<footer class="main-footer">
 		<div class="container">
 			<div class="row">
 				<div class="col-9">
